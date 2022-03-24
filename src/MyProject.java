974226f55beb3ce4778606f5115f0aeae7c353df
IlliyaF <yiliy749@gmail.com>
2022-03-24 18:44:54 +0200

    Until study gitHub

--- src/MyProject.java
+++ src/MyProject.java
@@ -1,5 +1,7 @@
 public class MyProject {
     public static void main(String[] args) {
         System.out.println("My project is ready");
+
+        System.out.println("I am study gitHub");
     }
 }
